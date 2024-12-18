18d173352ca2461396e8a0fe2941ce8e31c8ef1e
praveen_nw18 <sharmapraveen.1dec@gmail.com>
2024-12-18 20:00:53 +0530

    two sum

---
+++ src/Java_Programmes/Two_no/two_sum.java
@@ -0,0 +1,26 @@
+package Java_Programmes.Two_no;
+
+import java.util.Scanner;
+
+public class two_sum {
+    public static void main(String[] args) {
+        // Create a Scanner object for input
+        Scanner scanner = new Scanner(System.in);
+
+        // Prompt the user to enter two numbers
+        System.out.print("Enter the first number: ");
+        int num1 = scanner.nextInt();
+
+        System.out.print("Enter the second number: ");
+        int num2 = scanner.nextInt();
+
+        // Calculate the sum
+        int sum = num1 + num2;
+
+        // Display the result
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+
+        // Close the scanner
+        scanner.close();
+    }
+}
